34e36fb1ed937160ddeee351386a8e46ea0be12c
Sarah Labrosse <59272638+Slabrosse@users.noreply.github.com>
2020-11-19 11:34:59 -0500

    created R3.1-R3.7

--- CI-Python/qa327_test/frontend/test_registraion.py
+++ CI-Python/qa327_test/frontend/test_registraion.py
@@ -3,7 +3,7 @@ from seleniumbase import BaseCase
 
 from qa327_test.conftest import base_url
 from unittest.mock import patch
-from qa327.models import db, User
+from qa327.models import db, User, Form
 from werkzeug.security import generate_password_hash, check_password_hash
 
 """
@@ -21,18 +21,26 @@ the user from the database.
 Annotate @patch before unit tests can mock backend methods (for that testing function)
 """
 
-# Moch a sample user
+# Mock a sample user
 test_user = User(
     email='test_frontend@test.com',
     name='test_frontend',
-    password=generate_password_hash('Test_frontend@')
+    password=generate_password_hash('Test_frontend@'),
+    balance=500
 )
 
-# Moch some sample tickets
+# Mock some sample tickets
 test_tickets = [
-    {'name': 't1', 'price': '100'}
+    {'name': 't1', 'price': '100', 'quantity': '2', 'email': 'test123@email.com', 'date': '02/23/2020'}
 ]
 
+# Mock a sample buy form
+test_sell_form = Form(
+    name='buy_tix',
+    quantity='2',
+    price='50',
+    date='02/23/2020'
+)
 
 class FrontEndHomePageTest(BaseCase):
 
@@ -41,7 +49,8 @@ class FrontEndHomePageTest(BaseCase):
     def test_login_success(self, *_):
         """
         This is a sample front end unit test to login to home page
-        and verify if the tickets are correctly listed.
+        and verify if the tickets are correctly listed. It also
+        checks that the name, price, quantity, email and date are all listed
         """
         # open login page
         self.open(base_url + '/login')
@@ -65,9 +74,9 @@ class FrontEndHomePageTest(BaseCase):
         self.open(base_url)
         # test if the page loads correctly
         self.assert_element("#welcome-header")
-        self.assert_text("Welcome test_frontend", "#welcome-header")
+        self.assert_text("Hi test_frontend", "#welcome-header")
         self.assert_element("#tickets div h4")
-        self.assert_text("t1 100", "#tickets div h4")
+        self.assert_text("t1 100 2 test123@email.com 02/23/2020", "#tickets div h4")
 
     @patch('qa327.backend.get_user', return_value=test_user)
     @patch('qa327.backend.get_all_tickets', return_value=test_tickets)
@@ -93,4 +102,91 @@ class FrontEndHomePageTest(BaseCase):
         #make sure it shows the proper page and message
         self.assert_element("#message")
         self.assert_text("Please login", "#message")
+
+    @patch('qa327.backend.get_user', return_value=test_user)
+    def test_show_header(self, *_):
+        """Validate that this page shows a header ‘Hi {}’.format(user.name)"""
+        # Open the logout page to invalidate any logged-in session
+        self.open(base_url + '/logout')
+        # open the login page
+        self.open(base_url + '/login')
+        self.type("#email", "test_frontend@test.com")
+        self.type("#password", "Test_frontend@")
+        # click enter button
+        self.click('input[type="submit"]')
+        # open home page
+        self.open(base_url)
+        # This page shows a header ‘Hi {}’.format(user.name)
+        self.assert_element("#welcome-header")
+        self.assert_text("Hi test_frontend", "#welcome-header")
+
+    @patch('qa327.backend.get_user', return_value=test_user)
+    def test_balance(self, *_):
+        """Validate that this page shows user balance"""
+        # Open the logout page to invalidate any logged-in session
+        self.open(base_url + '/logout')
+        # open the login page
+        self.open(base_url + '/login')
+        self.type("#email", "test_frontend@test.com")
+        self.type("#password", "Test_frontend@")
+        # click enter button
+        self.click('input[type="submit"]')
+        # open home page
+        self.open(base_url)
+        self.assert_element("#user-balance")
+        self.assert_text("Your current balance is: 500", "#user-balance")
+
+    @patch('qa327.backend.get_user', return_value=test_user)
+    def test_show_logout(self, *_):
+        """Validate that there is a logout link"""
+        # Open the logout page to invalidate any logged-in session
+        self.open(base_url + '/logout')
+        # open the login page
+        self.open(base_url + '/login')
+        self.type("#email", "test_frontend@test.com")
+        self.type("#password", "Test_frontend@")
+        # click enter button
+        self.click('input[type="submit"]')
+        # open home page
+        self.open(base_url)
+        self.assert_element("#logout-link")
+
+    @patch('qa327.backend.get_user', return_value=test_user)
+    def test_sell_buy(self, *_):
+        """Validate buy form and fields name, quantity, price, exp date exist"""
+        # Open the logout page to invalidate any logged-in session
+        self.open(base_url + '/logout')
+        # open the login page
+        self.open(base_url + '/login')
+        self.type("#email", "test_frontend@test.com")
+        self.type("#password", "Test_frontend@")
+        # click enter button
+        self.click('input[type="submit"]')
+        # open home page
+        self.open(base_url)
+        self.assert_element("#name_sell")
+        self.assert_element("#quantity_sell")
+        self.assert_element("#price_sell")
+        self.assert_element("#exp_date_sell")
+
+    @patch('qa327.backend.get_user', return_value=test_user)
+    def test_form_buy(self, *_):
+        """Validate buy form and fields name, quantity, price, exp date exist"""
+        # Open the logout page to invalidate any logged-in session
+        self.open(base_url + '/logout')
+        # open the login page
+        self.open(base_url + '/login')
+        self.type("#email", "test_frontend@test.com")
+        self.type("#password", "Test_frontend@")
+        # click enter button
+        self.click('input[type="submit"]')
+        # open home page
+        self.open(base_url)
+        self.assert_element("#name_buy")
+        self.assert_element("#quantity_buy")
+
+
+
+
+
 
