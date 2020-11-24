88e6f96cb42a8f5841d9eb511ba69879652860b7
Michael Fourie <57454725+michaelfourie@users.noreply.github.com>
2020-11-24 10:40:52 -0500

    removed duplicate

--- CI-Python/qa327/models.py
+++ CI-Python/qa327/models.py
@@ -60,17 +60,6 @@ class Ticket(db.Model):
         self.instances.append(self)  # allows this class to be in the form of an iterable
                                     # list of objects of type Ticket
 
-class Form(db.Model):
-    """
-    A form model which hold form information
-    """
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)  # owner of ticket
-    name = db.Column(db.String(1000))               # name of ticket
-    quantity = db.Column(db.Integer)                # quantity of this ticket
-    price = db.Column(db.Integer)                   # price of ticket of this type
-    date = db.Column(db.String(50))                 # expiration date
-
 
 # it creates all the SQL tables if they do not exist
 with app.app_context():
